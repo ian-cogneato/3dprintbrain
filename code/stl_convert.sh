93244d2a736e2ed46b787b34a23cd4cba300116e
Ian Dalton <ianad@lighthall-imac.local>
2018-02-06 10:39:32 -0500

    update to code/stl_convert.sh

--- code/stl_convert.sh
+++ code/stl_convert.sh
@@ -8,30 +8,26 @@
 # Replace the path below with your local path to the directory you
 # have the tools installed, or where you want to clone them
 export APPS_DIR=/volumes/lighthall_lab/apps
+export CODE_DIR="${0%/*}"
+
+cd $APPS_DIR/vcglib/apps/tridecimator
+qmake tridecimator.pro
+make
+cd $CODE_DIR
 
 
-mg=%APPS_DIR/meshgeometry/meshgeometry_mac
-
-ml=%APPS_DIR/meshlab.app/Contents/MacOS/meshlabserver
-
+mg=%APPS_DIR/meshgeo/meshgeometry_mac
 td=%APPS_DIR/vcglib/apps/tridecimator/tridecimator
+ml=/applications/meshlab.app/Contents/MacOS/meshlabserver
 
- 
+
 
 f=rh.pial
-
 $mg -i $f -centre -o $f.ply
-
 $td $f.ply $f.dec.ply 100000
-
 $ml -i $f.dec.ply -o $f.dec.stl
 
- 
-
 f=lh.pial
-
 $mg -i $f -centre -o $f.ply
-
 $td $f.ply $f.dec.ply 100000
-
 $ml -i $f.dec.ply -o $f.dec.stl
