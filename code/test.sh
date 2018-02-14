159e194823f137ead7bd3ce673c59b2af6e98297
ianad <ian.dalton@ucf.edu>
2018-02-13 19:52:35 -0500

    minor changes

--- code/test.sh
+++ code/test.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-echo $0
+echo test1
+echo test2
 
-mydir="${0%/*}"
-
-echo $mydir
-
-export CODE_DIR="${0%/*}"
-export SOURCE_DIR=$CODE_DIR/../sourcedata
-export DERIVS_DIR=$CODE_DIR/../derivatives
-export SUBJ=sub-0
-
-echo code_dir $CODE_DIR
-echo src_dir $SOURCE_DIR
-echo drv_dir $DERIVS_DIR
-
-cd $SOURCE_DIR
+read -p "Press ENTER to continue"
